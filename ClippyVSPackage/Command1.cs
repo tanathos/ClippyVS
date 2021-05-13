73654ca10e82b411f7d79d669880c462a57fd54d
Denis Abt <denis.abt@hotmail.com>
2021-05-13 07:40:57 +0200

    Final Target fixes, now targeting 2019 only

--- ClippyVSPackage/Command1.cs
+++ ClippyVSPackage/Command1.cs
@@ -44,29 +44,6 @@ namespace Recoding.ClippyVSPackage
         }
 
         /// <summary>
-        /// Gets the instance of the command.
-        /// </summary>
-        public static Command1 Instance
-        {
-            get;
-            private set;
-        }
-
-        /// <summary>
-        /// Initializes the singleton instance of the command.
-        /// </summary>
-        /// <param name="package">Owner package, not null.</param>
-        public static async Task InitializeAsync(AsyncPackage package)
-        {
-            // Switch to the main thread - the call to AddCommand in Command1's constructor requires
-            // the UI thread.
-            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
-
-            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)).ConfigureAwait(false) as OleMenuCommandService;
-            Instance = new Command1(package, commandService);
-        }
-
-        /// <summary>
         /// This function is the callback used to execute the command when the menu item is clicked.
         /// See the constructor to see how the menu item is associated with this function using
         /// OleMenuCommandService service and MenuCommand class.
